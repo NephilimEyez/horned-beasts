68c4a421e425a99b095b5e0b35fec00990e5d377
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-06-06 18:31:17 -0400

    Added a favorite and likes, whoops

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,19 +1,53 @@
 import React from 'react';
 
+
 class HornedBeast extends React.Component {
     constructor(props){
         super(props);
-
+        
         this.state = {
             likes: 0,
             fav: false
         }
     }
+    
+    favorite = () => {
+        this.setState({
+            fav: true
+        })
+    }
+
+    unFavorite = () => {
+        this.setState({
+            fav: false
+        })
+    }
+
+    favHandler = () => {
+        if(this.state.fav === true){
+            return <p onClick={this.unFavorite}>💔 Unfavorite me.</p>
+        } else {
+            return <p onClick={this.favorite}>❤️ Favorite me!</p>
+        }
+    }
+
+    liked = () => {
+        this.setState({
+            likes: this.state.likes +1
+        })
+    }
+
     render(){
         return (
             <>
-            <article>
+            <article className={this.state.fav ? 'favorite' : ''}>
+                <h2>{this.props.name}</h2>
+                <div>
+                    <p onClick={this.liked}>👍 Like me!</p>
+                    {this.favHandler()}
+                </div>
                 <img title={this.props.title} alt={this.props.description} src={this.props.image_url} />
+                <p>Liked: {this.state.likes} times</p>
             </article>
             </>
         )
