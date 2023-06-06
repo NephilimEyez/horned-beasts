698926be42068a9d90ab1654efc65b94e3d32922
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-06-06 19:42:22 -0400

    Added a key value to main map

--- src/Main.js
+++ src/Main.js
@@ -13,8 +13,8 @@ class Main extends React.Component {
     render() {
         return (
             <main>
-                {data.map(animalObj => {
-                    return <HornedBeast name={animalObj.title} title={animalObj.keyword} image_url={animalObj.image_url} description={animalObj.description} horns={animalObj.horns} />
+                {data.map((animalObj, idx) => {
+                    return <HornedBeast key={idx} name={animalObj.title} title={animalObj.keyword} image_url={animalObj.image_url} description={animalObj.description} horns={animalObj.horns} />
                 })}
             </main>
         )
